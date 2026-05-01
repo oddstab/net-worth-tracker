9e28909fba1bf9a4e6948e75399cd727292d71e8
oddstab <oddstab@gmail.com>
2026-05-02 05:04:31 +0800

    chore: remove legacy vanilla JS/HTML files, SvelteKit is now the sole codebase

--- find-error.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('./js/ui/modal.js', 'utf8');
-
-try {
-  new Function(content.replace(/^import .+$/mg, '').replace(/^export /mg, ''));
-  fs.writeFileSync('./result.txt', 'No syntax errors\n');
-} catch(e) {
-  fs.writeFileSync('./result.txt', 'Error: ' + e.message + '\n');
-}
